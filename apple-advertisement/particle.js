dd1056bc411ec0b80755ecd646e6c8bb163ef669
inyoung <rmdnps10@gmail.com>
2023-11-28 00:18:20 +0900

    Feat: 머신러닝 라이브러리 적용

--- apple-advertisement/particle.js
+++ apple-advertisement/particle.js
@@ -1,101 +1,101 @@
-function Particle(x, y) {
-  this.pos = new p5.Vector(x, y);
-  this.vel = new p5.Vector(0, 0);
-  this.acc = new p5.Vector(0, 0);
-  this.target = new p5.Vector(0, 0);
-  this.isKilled = false;
-  this.maxSpeed = random(0.25, 2); 
-  this.maxForce = random(8, 15); 
-  this.currentColor = color(0);
-  this.endColor = color(0);
-  this.colorBlendRate = random(0.01, 0.05);
-  this.currentSize = 0;
-  this.distToTarget = 0;
-  this.move = function () {
-    this.distToTarget = dist(
-      this.pos.x,
-      this.pos.y,
-      this.target.x,
-      this.target.y
-    );
+  function Particle(x, y) {
+    this.pos = new p5.Vector(x, y);
+    this.vel = new p5.Vector(0, 0);
+    this.acc = new p5.Vector(0, 0);
+    this.target = new p5.Vector(0, 0);
+    this.isKilled = false;
+    this.maxSpeed = random(0.25, 2); 
+    this.maxForce = random(8, 15); 
+    this.currentColor = color(0);
+    this.endColor = color(0);
+    this.colorBlendRate = random(0.01, 0.05);
+    this.currentSize = 0;
+    this.distToTarget = 0;
+    this.move = function () {
+      this.distToTarget = dist(
+        this.pos.x,
+        this.pos.y,
+        this.target.x,
+        this.target.y
+      );
 
-    if (this.distToTarget < closeEnoughTarget) {
-      var proximityMult = this.distToTarget / closeEnoughTarget;
-      this.vel.mult(0.9);
-    } else {
-      var proximityMult = 1;
-      this.vel.mult(0.95);
-    }
-
-    if (this.distToTarget > 1) {
-      var steer = new p5.Vector(this.target.x, this.target.y);
-      steer.sub(this.pos);
-      steer.normalize();
-      steer.mult(this.maxSpeed * proximityMult * speedSlider.slider.value());
-      this.acc.add(steer);
-    }
-
-    var mouseDist = dist(this.pos.x, this.pos.y, mouseX, mouseY);
-
-    if (mouseDist < mouseSizeSlider.slider.value()) {
-      if (mouseIsPressed) {
-        var push = new p5.Vector(mouseX, mouseY);
-        push.sub(new p5.Vector(this.pos.x, this.pos.y));
+      if (this.distToTarget < closeEnoughTarget) {
+        var proximityMult = this.distToTarget / closeEnoughTarget;
+        this.vel.mult(0.9);
       } else {
-        var push = new p5.Vector(this.pos.x, this.pos.y);
-        push.sub(new p5.Vector(mouseX, mouseY));
+        var proximityMult = 1;
+        this.vel.mult(0.95);
       }
-      push.normalize();
-      push.mult((mouseSizeSlider.slider.value() - mouseDist) * 0.05);
-      this.acc.add(push);
-    }
-    this.vel.add(this.acc);
-    this.vel.limit(this.maxForce * speedSlider.slider.value());
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-  };
 
-  this.draw = function () {
-    this.currentColor = lerpColor(
-      this.currentColor,
-      this.endColor,
-      this.colorBlendRate
-    );
-    stroke(this.currentColor);
-    if (!this.isKilled) {
-      var targetSize = map(
-        min(this.distToTarget, closeEnoughTarget),
-        closeEnoughTarget,
-        0,
-        0,
-        particleSizeSlider.slider.value()
+      if (this.distToTarget > 1) {
+        var steer = new p5.Vector(this.target.x, this.target.y);
+        steer.sub(this.pos);
+        steer.normalize();
+        steer.mult(this.maxSpeed * proximityMult * speedSlider.slider.value());
+        this.acc.add(steer);
+      }
+
+      var mouseDist = dist(this.pos.x, this.pos.y, mouseX, mouseY);
+
+      if (mouseDist < mouseSizeSlider.slider.value()) {
+        if (mouseIsPressed) {
+          var push = new p5.Vector(mouseX, mouseY);
+          push.sub(new p5.Vector(this.pos.x, this.pos.y));
+        } else {
+          var push = new p5.Vector(this.pos.x, this.pos.y);
+          push.sub(new p5.Vector(mouseX, mouseY));
+        }
+        push.normalize();
+        push.mult((mouseSizeSlider.slider.value() - mouseDist) * 0.05);
+        this.acc.add(push);
+      }
+      this.vel.add(this.acc);
+      this.vel.limit(this.maxForce * speedSlider.slider.value());
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+    };
+
+    this.draw = function () {
+      this.currentColor = lerpColor(
+        this.currentColor,
+        this.endColor,
+        this.colorBlendRate
       );
-    } else {
-      var targetSize = 2;
-    }
-    this.currentSize = lerp(this.currentSize, targetSize, 0.1);
-    strokeWeight(this.currentSize);
-    point(this.pos.x, this.pos.y);
-  };
+      stroke(this.currentColor);
+      if (!this.isKilled) {
+        var targetSize = map(
+          min(this.distToTarget, closeEnoughTarget),
+          closeEnoughTarget,
+          0,
+          0,
+          particleSizeSlider.slider.value()
+        );
+      } else {
+        var targetSize = 2;
+      }
+      this.currentSize = lerp(this.currentSize, targetSize, 0.1);
+      strokeWeight(this.currentSize);
+      point(this.pos.x, this.pos.y);
+    };
 
-  this.kill = function () {
-    if (!this.isKilled) {
-      this.target = generateRandomPos(
-        width / 2,
-        height / 2,
-        max(width, height)
+    this.kill = function () {
+      if (!this.isKilled) {
+        this.target = generateRandomPos(
+          width / 2,
+          height / 2,
+          max(width, height)
+        );
+        this.endColor = color(0);
+        this.isKilled = true;
+      }
+    };
+
+    this.isOutOfBounds = function () {
+      return (
+        this.pos.x < 0 ||
+        this.pos.x > width ||
+        this.pos.y < 0 ||
+        this.pos.y > height
       );
-      this.endColor = color(0);
-      this.isKilled = true;
-    }
-  };
-
-  this.isOutOfBounds = function () {
-    return (
-      this.pos.x < 0 ||
-      this.pos.x > width ||
-      this.pos.y < 0 ||
-      this.pos.y > height
-    );
-  };
-}
+    };
+  }
--- apple-advertisement/sketch.js
+++ apple-advertisement/sketch.js
@@ -1,6 +1,9 @@
 // 20201148 정인영
-// Advertising about Apple branding 
+// Advertising about Apple branding
 
+let poseNet;
+let poses = [];
+let handIsUp = false; // Variable to track hand position
 var imgs = [];
 var imgNames = [
   "assets/logo/1977.png",
@@ -31,9 +34,20 @@ function preload() {
   sfFontMedium = loadFont("assets/font/SFPRODISPLAYMEDIUM.OTF");
 }
 
+function modelReady() {
+  select("#status").html("Model Loaded");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // Create on-screen controls.
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  poseNet = ml5.poseNet(video, modelReady);
+  poseNet.on("pose", function (results) {
+    poses = results;
+  });
+  video.hide();
   mouseSizeSlider = new SliderLayout("Mouse size", 50, 200, 100, 1, 100, 100);
 
   particleSizeSlider = new SliderLayout(
@@ -72,6 +86,56 @@ function setup() {
   nextImage();
 }
 
+function modelLoaded() {
+  console.log("PoseNet model loaded");
+}
+function gotPoses(results) {
+  poses = results;
+  console.log(poses);
+  if (poses.length > 0) {
+    // Check if the hand is raised (you may need to adjust the keypoints index)
+    handIsUp = poses[0].pose.keypoints[9].position.y < windowHeight / 2;
+  }
+}
+
+function drawKeypoints() {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(
+        partA.position.x,
+        partA.position.y,
+        partB.position.x,
+        partB.position.y
+      );
+    }
+  }
+}
+
 function draw() {
   background(0);
 
@@ -98,6 +162,14 @@ function draw() {
   textSize(20);
   textFont(sfFontRegular);
   text("History of Innovation.", windowWidth / 2, (windowHeight / 12) * 11);
+  if (
+    poses.length > 0 &&
+    poses[0].pose.keypoints[0].position.y < windowHeight / 2
+  ) {
+    nextImage();
+  }
+
+  image(video, (width / 5) * 4, 0, width / 5, height / 3);
 }
 
 function keyPressed() {
